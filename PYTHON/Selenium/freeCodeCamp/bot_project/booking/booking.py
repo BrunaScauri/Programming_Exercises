097345d78d1ab4b9ed7d6a69a7a0203df3ebe4c3
BrunaScauri <brunascauri@hotmail.com>
2022-10-11 19:35:42 -0300

    feat: add context manager to self exit after loading page

--- PYTHON/Selenium/freeCodeCamp/bot_project/booking/booking.py
+++ PYTHON/Selenium/freeCodeCamp/bot_project/booking/booking.py
@@ -3,9 +3,14 @@ from selenium import webdriver
 
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r'../../../drivers'):
+    def __init__(self, driver_path=r'../../../drivers', teardown=False):
         self.driver_path = driver_path
+        self.teardown = teardown
         super(Booking, self).__init__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.teardown:
+            self.quit()
 
     def land_first_page(self):
         self.get(const.BASE_URL)
