c2d435bf499817d2504efccbfee7b69d2018d1a1
BrunaScauri <brunascauri@hotmail.com>
2022-10-19 19:03:24 -0300

    feat: filtration by number of adults and dates bug fixed

--- PYTHON/Selenium/freeCodeCamp/bot_project/run.py
+++ PYTHON/Selenium/freeCodeCamp/bot_project/run.py
@@ -4,4 +4,7 @@ with Booking() as bot:
   bot.land_first_page()
   bot.change_currency(currency='USD')
   bot.select_place_to_go('New York')
-  bot.select_date(check_in_date="2022-10-16", check_out_date="2022-10-23")
+  bot.select_date(check_in_date="2022-10-20", 
+  check_out_date="2022-10-25")
+  bot.select_adults(10)
+  bot.click_search()
